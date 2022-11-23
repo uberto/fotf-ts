72de092b1e9ba208178e3af471947225008ecebe
Uberto Barbini <uberto.gama@gmail.com>
2022-11-23 12:12:51 +0900

    add comments

--- src/http.ts
+++ src/http.ts
@@ -1,5 +1,13 @@
 import { RouteParams, routing, jsonResponse } from './routes'
 
+//todo:
+//separate the library from the app files
+//rename http.ts to todolistApp
+//url for all list for users /:user/lists
+//url for list details /:user/list/:id
+//nicer DSL to create routes
+
+
 // to test: curl http://localhost:3000/pippo/lists/1234
 const r1: RouteParams = {
   // do we really need the req here? maybe just a string | regex?
@@ -11,7 +19,18 @@ const r1: RouteParams = {
 }
 
 
-const myRoutes = routing([r1])
+const myRoutes = routing([
+    r1
+])
+
+// some kind of DSL like this:
+//
+// const myRoutes = routing([
+//   "/:user/lists" GET { allListsForUser(u)},
+//   "/:user/list/:id" GET { getListForUser(u, id)}
+//   "/:user/list/:id" PUT { updateListForUser(u, id, list)}
+//   "/:user/list" POST { newListForUser(u, list)}
+// ])
 
 const logRequest = (request: Request) => {
   console.log('REQUEST:')
